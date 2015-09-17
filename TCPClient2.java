754e9bf998b3be36a8b8db8f3ba914b95e09941d
Uswatun Hasanah <uswatun.2510@gmail.com>
2015-09-17 11:08:06 +0700

    Create TCPClient2.java

---
+++ TCPClient2.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.net.*;
+import java.lang.*;
+
+class TCPClient 
+{
+ public static void main(String argv[]) throws Exception
+ {
+  boolean isExit = false;
+  Socket clientSocket = new Socket("localhost", 6789);
+  String sentence;
+  String modifiedSentence;
+  
+  while(!(isExit)) 
+   {  
+  BufferedReader inFromUser = new BufferedReader(
+     new InputStreamReader(System.in));
+  DataOutputStream outToServer = new DataOutputStream(
+     clientSocket.getOutputStream());
+  BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+	 
+  sentence = inFromUser.readLine();
+  outToServer.writeBytes(sentence+"\n");
+  modifiedSentence = inFromServer.readLine();
+  System.out.println("FROM SERVER: " + 
+          modifiedSentence);
+	if(sentence.equals("1")){
+	
+	byte[] mybytearray = new byte[1024];
+    InputStream is = clientSocket.getInputStream();
+    FileOutputStream fos = new FileOutputStream("fileSent1.txt");
+    BufferedOutputStream bos = new BufferedOutputStream(fos);
+    int bytesRead = is.read(mybytearray, 0, mybytearray.length);
+    bos.write(mybytearray, 0, bytesRead);
+    bos.close();
+	sentence = "0";
+	
+	}
+	else if(sentence.equals("2")){
+		byte[] mybytearray = new byte[1024];
+		InputStream is = clientSocket.getInputStream();
+		FileOutputStream fos = new FileOutputStream("fileSent2.txt");
+		BufferedOutputStream bos = new BufferedOutputStream(fos);
+		int bytesRead = is.read(mybytearray, 0, mybytearray.length);
+		bos.write(mybytearray, 0, bytesRead);
+		bos.close();
+		sentence = "0";
+	}
+	else if(sentence.equals("3")){
+		byte[] mybytearray = new byte[1024];
+		InputStream is = clientSocket.getInputStream();
+		FileOutputStream fos = new FileOutputStream("fileSent3.txt");
+		BufferedOutputStream bos = new BufferedOutputStream(fos);
+		int bytesRead = is.read(mybytearray, 0, mybytearray.length);
+		bos.write(mybytearray, 0, bytesRead);
+		bos.close();
+		sentence = "0";
+	}
+	}
+  clientSocket.close();
+ }
+}
